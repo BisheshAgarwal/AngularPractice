b118dd565f40a96afcd2859063002061bac5ac0e
BisheshAgarwal <52568056+BisheshAgarwal@users.noreply.github.com>
2021-03-12 20:23:40 +0530

    Added delete functionality

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -3,8 +3,13 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class CartService {
   items = [];
+  total: number;
   addToCart(product) {
     this.items.push(product);
+  }
+  removeFromCart(id) {
+    const newItems = this.items.filter(item => item.id != id);
+    this.items = newItems;
   }
   getItems() {
     return this.items;
@@ -12,6 +17,10 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+  getTotal() {
+    this.total = this.items.reduce((acc, curr) => acc + curr.price, 0);
+    return this.total;
   }
   constructor() {}
 }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -13,16 +13,19 @@ export class CartComponent {
     name: "",
     address: ""
   });
+  total = this.cart.getTotal();
 
   formSubmitHandler() {
     this.items = this.cart.clearCart();
+    window.alert("Check console");
     console.log("Successfully ordered", this.checkOutForm.value);
     this.checkOutForm.reset();
   }
 
   removeItemHandler(id: number) {
-    const newItems = this.items.filter(item => item.id != id);
-    this.items = newItems;
+    this.cart.removeFromCart(id);
+    this.items = this.cart.getItems();
+    this.total = this.cart.getTotal();
   }
 
   constructor(private cart: CartService, private formBuilder: FormBuilder) {}
